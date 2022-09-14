5a0d137f96a6b769c63ea3c87432a7366ee81094
Roberta Parisi <r.parisi@ilivetech.it>
2022-09-14 16:02:47 +0200

    dict_path_utils aggiornata

--- loko_orchestrator/utils/dict_path_utils.py
+++ loko_orchestrator/utils/dict_path_utils.py
@@ -1,5 +1,5 @@
-from dict_path import DictPath
 
+from dict_path import DictPath
 
 class SafeDictPath(DictPath):
     def __contains__(self, path):
@@ -13,3 +13,49 @@ class SafeDictPath(DictPath):
                 return False
             current = current[attr]
         return True
+
+    def __delitem__(self, path):
+        orig_path = self.clean_path(path)
+        path = orig_path[:-1]
+        args = orig_path[-1]
+        current = self.data
+        for attr in path:
+            current = current[attr]
+
+        del current[args]
+
+
+
+
+if __name__ == '__main__':
+
+
+
+    # test_dict = {'foo1': {'foo2': {'foo3': ["1", "2"]}}}
+    test_dict = {'foo1': {'foo2': {'foo3': {'foo4': 'bar'}}, "mila":[1, 2, 3]}, "cua":1, "clua":"pp", "ccc":2}
+
+    data = SafeDictPath(test_dict)
+    print(data.clean_path('foo1/foo2/foo3/foo4'))
+    print(data.get('foo1/foo2/foo3/foo4'))  # result: bar
+    # set value with easy
+    data.set('foo1/foo2/foo3/foo5', 'bar1')
+    data.get('foo1/foo2/foo3/foo5')  # result: bar1
+    # print(data)
+    del data["foo1/foo2/foo4"]
+
+    # data.__delitem__()
+    print(data)
+    # data.__delitem__()
+    #
+
+
+    # f(test_dict, ['foo1'])
+    # print(test_dict)
+    #
+    ## test  __contains__ function
+    # print("foo" in data)
+    # print("foo1/foo3" in data)
+    # print("foo1/foo2/foo5" in data)
+    # print("foo1/foo2" in data)
+    # print("foo1/foo2/foo3" in data)
+
